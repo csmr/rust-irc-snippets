c0cbd58a08e4f6ac9df3a8635f6526d3ae8b7e24
Casimir <csmr@users.noreply.github.com>
2021-08-21 21:12:52 +0300

    Update irc-ipc2-read-match.rs
    Update match clause

--- irc-ipc2-read-match.rs
+++ irc-ipc2-read-match.rs
@@ -1,4 +1,9 @@
-let irc_commpacketet = match irc_ipc2_rx.try_recv() {
-    Result => println!("yep"),
-    TryRecvError => println!("Unhandled match")
-}
+let irc_commpacket = match irc_ipc2_rx.try_recv() {
+    Ok(irc_commpacket_rx) => {
+        println!("Event recieved: {}",irc_commpacket_rx.event);
+    }
+    Err(error) => {
+        println!("Error caught: {}",error);
+    }
+};
+thread::sleep(_DURATION_1000MS);
