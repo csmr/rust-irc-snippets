02cfe85ae2b6d529d1ca7b813aaf56592d799b91
Casimir <csmr@users.noreply.github.com>
2021-08-21 21:17:51 +0300

    Update simple-game-server.rs
    Update feature list

--- simple-game-server.rs
+++ simple-game-server.rs
@@ -1,1 +1,10 @@
-
+// Game Server - Simple indices of each game instance's players state
+//
+// * session-server addr
+// * gamerules object-instance -- maxspeed, world bb
+// * player-index, composed of sessionkey, vectors 3Dposition && 3Ddirection
+//
+// - logic for requesting game-session object = rules, playerlist.
+// - logic for checking player session validity.
+// - logic for bounds-checking for player-submitted move
+// - logic for (http/udp) api endpoint SubmitPlayerMove, refresh 10 000 ms
